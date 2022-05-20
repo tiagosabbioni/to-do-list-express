afb6ccfbe61cd2c36e20f38b7d8f7c1bd59e2350
Tiago Sabioni <tiagobressar@gmail.com>
2022-05-20 18:33:58 -0300

    feat: Adição do Bulma e rotas de listagem de checklists e tasks

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 "use strict";
 
 const express = require("express");
-const checklistRouter = require("./src/routes/checklist");
+const path = require("path");
+
+const checklistsRouter = require("./src/routes/checklist");
+const rootRouter = require("./src/routes/index");
 require("./config/database");
+
 
 const app = express();
 
-app.use(express.json());
-app.use("/checklist", checklistRouter);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "src/views"));
+app.set("view engine", "ejs");
+
+app.use("/", rootRouter);
+app.use("/checklists", checklistsRouter);
 
 app.listen(3000, () => {
     console.log("Servidor Iniciado");
